7c41c54176ffd17f1979633801bdc70ef220a311
nachool <nachma.olivia1394@gmail.com>
2024-04-23 20:01:21 +0700

    menambah prosedur display

--- IntoOOP2/IntoOOP2.cpp
+++ IntoOOP2/IntoOOP2.cpp
@@ -20,6 +20,12 @@ public:
 	float hitungLuas(){
 		return panjang * lebar;
 	}
+
+	void display() {
+		cout << "Panjangnya = " << panjang << endl;
+		cout << "Lebarnya = " << lebar << endl;
+		cout << "Luasnya = " << hitungLuas() << endl;
+	}
 };
 
 int main()
